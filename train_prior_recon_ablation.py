c87322caf9da37832fa9d1fa959f5342e4c35383
enfi3141 <kwanyh031110@cau.ac.kr>
2026-05-30 17:55:13 +0900

    update ablation code

--- train_prior_recon_ablation.py
+++ train_prior_recon_ablation.py
@@ -408,6 +408,15 @@ def build_model(args, meta_in_dim):
             use_meta=False,
             meta_in_dim=meta_in_dim,
         )
+    
+    elif args.experiment == "raw1_meta":
+        return SingleECGClassifier(
+            input_key="raw_1lead",
+            in_channels=1,
+            num_classes=args.num_classes,
+            use_meta=True,
+            meta_in_dim=meta_in_dim,
+        )
 
     elif args.experiment == "recon12":
         return SingleECGClassifier(
@@ -688,6 +697,7 @@ def main():
         required=True,
         choices=[
             "raw1",
+            "raw1_meta",
             "recon12",
             "recon12_meta",
             "raw1_recon12",
